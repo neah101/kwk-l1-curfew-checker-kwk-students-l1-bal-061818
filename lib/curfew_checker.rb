354a81ddd9f03547207be3065ca17e5db5ceef1d
neah101 <neah.shaw2005@gmail.com>
2018-06-20 13:06:25 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -1,13 +1,16 @@
 def simple_curfew_checker(time)
-  # code goes here
+ if time == 11
+   puts "you are on time"
 end
 
 def curfew_checker(time)
-  # code goes here
+if time > 11
+  puts "you are late"
 end
 
 def complex_curfew_checker(time)
-  # code goes here
+  if time < 11
+    puts " you have time"
 end
 
 def deluxe_curfew_checker(time)
